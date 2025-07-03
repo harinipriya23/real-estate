a3523e5813b5465f5e99f2e31f31d4516f1c7aef
Harini priya <harinipriya200903@gmail.com>
2025-07-03 10:09:13 +0530

    grid card

--- frontend/src/components/ExploreProperty.jsx
+++ frontend/src/components/ExploreProperty.jsx
@@ -2,22 +2,27 @@ import { useContext } from "react";
 import { PropertyContext } from "../context/PropertyContext";
 
 export const ExploreProperty = () => {
-  const { properties, loading } = useContext(PropertyContext);
+  const { properties, loading, propertyData } = useContext(PropertyContext);
 
   if (loading) return <p>Loading properties...</p>;
 
   return (
-    <section className="min-h-svh w-full p-4 gap-4 bg-white text-black dark:bg-zinc-950 dark:text-zinc-100">
-      <div className="flex justify-between">
-        <p className="text-lg lg:text-2xl font-semibold font-poppins">
+    <section className="min-h-svh w-full  bg-neutral-100 rounded-xl text-black dark:bg-zinc-950 dark:text-zinc-100">
+      <div>
+        <p className="text-xl lg:text-2xl text-center font-bold font-poppins">
           Explore our Properties
         </p>
       </div>
-      <div>
-        {properties.map((item) => (
-          <div className="flex">
-            <p>{item.type}</p>
-            <img src={item.image} />
+      <div className="gap-4 p-4 md:px-6 md:gap-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 overflow-auto">
+        {propertyData.map((item) => (
+          <div key={item.id} className="rounded-lg row-span-2 flex-shrink-0">
+            <div>
+              <img
+                src={item.img}
+                alt={item.type}
+                className="h-[170px] md:h-[200px] w-full object-cover rounded"
+              />
+            </div>
           </div>
         ))}
       </div>
--- frontend/src/context/PropertyContext.jsx
+++ frontend/src/context/PropertyContext.jsx
@@ -10,6 +10,81 @@ export const PropertyProvider = ({ children }) => {
   const [properties, setProperties] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const propertyData = [
+    {
+      id: 0,
+      type: "Resort",
+      img: "https://images.unsplash.com/photo-1606402179428-a57976d71fa4?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "",
+    },
+    {
+      id: 1,
+      type: "Apartment",
+      img: "https://images.unsplash.com/photo-1608974007646-4343e2b83b77?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTF8fGFwYXJ0bWVudCUyMGJ1aWxkaW5nfGVufDB8fDB8fHww",
+      link: "",
+    },
+    {
+      id: 2,
+      type: "Villa",
+      img: "https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGx1eHVyeSUyMHZpbGxhfGVufDB8fDB8fHww",
+      link: "",
+    },
+    {
+      id: 3,
+      type: "Studio Apartment",
+      img: "https://images.unsplash.com/photo-1709145883296-4443310e3be0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fGFwYXJ0bWVudCUyMGJ1aWxkaW5nfGVufDB8fDB8fHww",
+      link: "",
+    },
+    {
+      id: 4,
+      type: "Row House",
+      img: "https://images.unsplash.com/photo-1651733834052-71680694092a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU3fHxyb3clMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D",
+      link: "",
+    },
+    {
+      id: 5,
+      type: "Penthouse",
+      img: "https://images.unsplash.com/photo-1625602812206-5ec545ca1231?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fGhvdXNlfGVufDB8fDB8fHww",
+      link: "",
+    },
+    {
+      id: 6,
+      type: "Farmhouse",
+      img: "https://images.unsplash.com/photo-1674306147096-d4c052b9816d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fHJlc29ydHxlbnwwfHwwfHx8MA%3D%3D",
+      link: "",
+    },
+    {
+      id: 7,
+      type: "Plot",
+      img: "https://images.pexels.com/photos/32798469/pexels-photo-32798469.jpeg",
+      link: "",
+    },
+    {
+      id: 8,
+      type: "Independent House",
+      img: "https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdXNlfGVufDB8fDB8fHww",
+      link: "",
+    },
+    {
+      id: 9,
+      type: "Townhouse",
+      img: "https://images.unsplash.com/photo-1705825533722-e562d3ee873c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fHRvd24lMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D",
+      link: "",
+    },
+    {
+      id: 10,
+      type: "Duplex",
+      img: "https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg",
+      link: "",
+    },
+    {
+      id: 11,
+      type: "Mansion",
+      img: "https://images.unsplash.com/photo-1548713740-2a50e43f0830?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "",
+    },
+  ];
+
   const fetchProperties = async () => {
     const url = "https://real-estate-4ama.onrender.com/properties";
     try {
@@ -28,7 +103,7 @@ export const PropertyProvider = ({ children }) => {
   }, []);
 
   return (
-    <PropertyContext.Provider value={{ properties, loading }}>
+    <PropertyContext.Provider value={{ properties, loading, propertyData }}>
       {children}
     </PropertyContext.Provider>
   );
