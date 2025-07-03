f9006781788b872fdfb01f44cce1fdaef2386275
Harini priya <harinipriya200903@gmail.com>
2025-07-03 12:55:38 +0530

    img added

--- frontend/src/components/ExploreProperty.jsx
+++ frontend/src/components/ExploreProperty.jsx
@@ -7,20 +7,20 @@ export const ExploreProperty = () => {
   if (loading) return <p>Loading properties...</p>;
 
   return (
-    <section className="min-h-svh w-full  bg-neutral-100 rounded-xl text-black dark:bg-zinc-950 dark:text-zinc-100">
+    <section className="min-h-svh w-full py-4 bg-neutral-100 rounded-xl text-black dark:bg-zinc-950 dark:text-zinc-100">
       <div>
         <p className="text-xl lg:text-2xl text-center font-bold font-poppins">
           Explore our Properties
         </p>
       </div>
-      <div className="gap-4 p-4 md:px-6 md:gap-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 overflow-auto">
+      <div className="gap-4 p-4 md:px-6 md:gap-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 overflow-auto">
         {propertyData.map((item) => (
-          <div key={item.id} className="rounded-lg row-span-2 flex-shrink-0">
+          <div key={item.id} className={`rounded-lg ${item.span}`}>
             <div>
               <img
                 src={item.img}
                 alt={item.type}
-                className="h-[170px] md:h-[200px] w-full object-cover rounded"
+                className="h-[170px] md:h-[280px] w-full aspect-square object-cover rounded"
               />
             </div>
           </div>
