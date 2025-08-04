7422f70eb16b05e42a9e152d6af419b0aca1b7fc
harinipriya23 <harinipriya200903@gmail.com>
2025-08-04 15:57:08 +0530

    mp 4

--- frontend/src/pages/PropertyDetails.jsx
+++ frontend/src/pages/PropertyDetails.jsx
@@ -28,21 +28,22 @@ export const PropertyDetails = () => {
                         alt={property.propertyName}
                         className="h-[250px] md:h-full w-full object-cover rounded-xl"
                     />
+                    <span className="absolute top-5 right-5" ><CallButton /></span>
                     <div className="absolute bottom-6 right-6 p-1 px-4 md:px-6 md:p-2 md:text-base text-sm bg-white text-black dark:bg-black dark:text-white rounded-md">
                         {property.status === "Buy" ? "For Sale" : "For Rent"}
                     </div>
                 </div>
                 <div className="col-span-6 md:pl-10">
-                  <div className="flex justify-between my-2">  <h2 className="text-xl md:text-2xl lg:text-3xl mt-2 font-bold text-center">
+                    <h2 className="text-xl md:text-2xl lg:text-3xl mt-2 font-bold text-center">
                         {property.propertyName}
-                    </h2><CallButton /></div>
+                    </h2>
                     <p className="font-normal md:text-base text-justify text-sm my-2">{property.description}</p>
-                   <div className="space-y-4"> <div className="my-4">
+                    <div className="space-y-4"> <div className="my-4">
                         <div className="font-semibold font-poppins space-y-2 w-full text-lg md:text-xl mb-2">
                             <span>Property Details</span>
                             <div className="h-[1px] bg-black dark:bg-neutral-300" />
                         </div>
-                        <div className="flex flex-wrap p-2 md:px-4 gap-2 font-semibold text-base md:text-lg ">
+                        <div className="flex flex-wrap p-2 md:px-4 gap-2 font-semibold text-sm md:text-base ">
                             <div>Property Name: <span className="font-normal">{property.propertyName}</span></div>
                             <div>Property Type: <span className="font-normal">{property.type}</span></div>
                             <div>Address: <span className="font-normal">{property.address}, {property.street} {property.location}</span></div>
@@ -56,36 +57,36 @@ export const PropertyDetails = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="my-4">
-                        <div className="w-full font-semibold font-poppins text-lg md:text-xl space-y-2 mb-2">
-                            <span>Contact Details</span>
-                            <div className="h-[1px] bg-black dark:bg-neutral-300" />
-                        </div>
-                            <div className="flex justify-around md:justify-between p-2 md:px-4  gap-2 font-semibold text-base md:text-lg ">
-                            <div>Name: <span className="font-normal">{property.owner.name}</span></div>
-                            <div>Phone: <span className="font-normal">+91 {property.owner.contact}</span></div>
-                        </div>
-                    </div>
-                    <div className="my-4">
-                        <div className="w-full font-semibold font-poppins text-lg md:text-xl space-y-2 mb-2">
-                            <span>Surroundings</span>
-                            <div className="h-[1px] bg-black dark:bg-neutral-300" />
-                        </div>
-                            <div className="flex flex-col p-2 md:px-4 font-semibold text-base md:text-lg ">
-                            <div>
-                                Landmarks:
-                                <span className="font-normal ml-1">
-                                    {property.nearbyLandmarks.join(", ")}
-                                </span>
+                        <div className="my-4">
+                            <div className="w-full font-semibold font-poppins text-lg md:text-xl space-y-2 mb-2">
+                                <span>Contact Details</span>
+                                <div className="h-[1px] bg-black dark:bg-neutral-300" />
                             </div>
-                            <div>
-                                Amenities:
-                                <span className="font-normal ml-1">
-                                    {property.amenities.join(", ")}
-                                </span>
+                            <div className="flex flex-col sm:flex-row sm:justify-between p-2 md:px-4  gap-2 font-semibold text-sm md:text-base ">
+                                <div>Name: <span className="font-normal">{property.owner.name}</span></div>
+                                <div>Phone: <span className="font-normal">+91 {property.owner.contact}</span></div>
                             </div>
                         </div>
-                    </div></div>
+                        <div className="my-4">
+                            <div className="w-full font-semibold font-poppins text-lg md:text-xl space-y-2 mb-2">
+                                <span>Surroundings</span>
+                                <div className="h-[1px] bg-black dark:bg-neutral-300" />
+                            </div>
+                            <div className="flex flex-col p-2 md:px-4 font-semibold text-sm md:text-base ">
+                                <div>
+                                    Landmarks:
+                                    <span className="font-normal ml-1">
+                                        {property.nearbyLandmarks.join(", ")}
+                                    </span>
+                                </div>
+                                <div>
+                                    Amenities:
+                                    <span className="font-normal ml-1">
+                                        {property.amenities.join(", ")}
+                                    </span>
+                                </div>
+                            </div>
+                        </div></div>
                 </div></div>
         </section>
     );
--- frontend/src/reusableComponents/DropDown.jsx
+++ frontend/src/reusableComponents/DropDown.jsx
@@ -9,7 +9,7 @@ export const DropDown = ({ options, onSelect, isOpen, isObject }) => {
     <ul className="absolute z-10 mt-2 w-44 bg-white text-black dark:bg-neutral-800 dark:text-neutral-300 dark:border-neutral-700 border border-neutral-200 rounded-md shadow-lg">
       {options.map((item, index) => (
         <li key={index} value={item.label} onClick={() => handleOption(item)}
-          className="px-4 py-2 hover:bg-blue-200 cursor-pointer"        >
+          className="px-4 py-2 hover:bg-neutral-200 dark:hover:bg-neutral-700 cursor-pointer"        >
           {item.label}
         </li>
       ))}
